f7cfdb9f8ef502fb5e243378acc3c835923f1996
Marcus Vinicius <5ucr4m@gmail.com>
2024-01-02 17:09:19 -0300

    chore: update game board

--- src/components/GameBoard/index.tsx
+++ src/components/GameBoard/index.tsx
@@ -1,23 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useMemo } from "react";
 import { Card } from "../Card";
 import { PlayerCard } from "../PlayerCard";
+import useSocket from "../../context/useSocket";
+import { useParams } from "react-router-dom";
 
 const fibonacciSeries = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, "?", "☕"];
 
 export const GameBoard: React.FC = () => {
-  const [selectedCard, setSelectedCard] = useState<string | number | null>(
-    null
-  );
+  const { id } = useParams<{ id: string }>();
+  const [selectedCard, setSelectedCard] = React.useState<string | number>("?");
+  const { infos, userID, sendValue, joinRoom } = useSocket();
+
+  const isSelected = useMemo(() => {
+    if (!infos) return false;
+
+    const myUser = infos.find((info) => info.userID === userID);
+
+    return !!myUser?.selected;
+  }, [infos, userID]);
+
+  useEffect(() => {
+    joinRoom(String(id));
+  }, [joinRoom, id]);
 
   const handleCardSelect = (value: string | number) => {
+    sendValue(value);
     setSelectedCard(value);
   };
 
   return (
     <>
       <div className="flex flex-col items-center justify-center p-4 h-full w-full ">
-        <div>
-          <PlayerCard selected={!!selectedCard} playerName="Marcus Soares" />
+        <div className="flex flex-row gap-10">
+          {infos.length > 0 && (
+            <PlayerCard
+              selected={infos[0].selected}
+              playerName={infos[0].username}
+            />
+          )}
+          {infos.length > 2 && (
+            <PlayerCard
+              selected={infos[2].selected}
+              playerName={infos[2].username}
+            />
+          )}
+          {infos.length > 4 && (
+            <PlayerCard
+              selected={infos[4].selected}
+              playerName={infos[4].username}
+            />
+          )}
+          {infos.length > 6 && (
+            <PlayerCard
+              selected={infos[6].selected}
+              playerName={infos[6].username}
+            />
+          )}
+          {infos.length > 8 && (
+            <PlayerCard
+              selected={infos[8].selected}
+              playerName={infos[8].username}
+            />
+          )}
         </div>
 
         <div className="flex items-center justify-center w-1/3 h-1/4 bg-white rounded-lg my-10">
@@ -26,8 +70,37 @@ export const GameBoard: React.FC = () => {
           </button>
         </div>
 
-        <div>
-          <PlayerCard selected={!!selectedCard} playerName="Marcus Soares" />
+        <div className="flex flex-row gap-10">
+          {infos.length > 1 && (
+            <PlayerCard
+              selected={infos[1].selected}
+              playerName={infos[1].username}
+            />
+          )}
+          {infos.length > 3 && (
+            <PlayerCard
+              selected={infos[3].selected}
+              playerName={infos[3].username}
+            />
+          )}
+          {infos.length > 5 && (
+            <PlayerCard
+              selected={infos[5].selected}
+              playerName={infos[5].username}
+            />
+          )}
+          {infos.length > 7 && (
+            <PlayerCard
+              selected={infos[7].selected}
+              playerName={infos[7].username}
+            />
+          )}
+          {infos.length > 9 && (
+            <PlayerCard
+              selected={infos[9].selected}
+              playerName={infos[9].username}
+            />
+          )}
         </div>
       </div>
       <div className="flex justify-center flex-wrap mt-auto mb-6">
@@ -35,7 +108,7 @@ export const GameBoard: React.FC = () => {
           <Card
             key={value.toString()}
             value={value}
-            selected={selectedCard === value}
+            selected={isSelected && selectedCard === value}
             onSelect={handleCardSelect}
           />
         ))}
