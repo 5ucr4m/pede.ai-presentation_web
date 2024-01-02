7d95f89ca3571a0f83eebfd118d14ccf017d4b58
Marcus Vinicius <5ucr4m@gmail.com>
2024-01-02 17:07:56 -0300

    chore: update form page

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -3,7 +3,7 @@ import GameForm from "../components/Form";
 export function FormPage() {
   return (
     <div className="flex flex-1 h-screen w-screen items-center justify-center">
-      <GameForm onCreateGame={() => {}} />{" "}
+      <GameForm />
     </div>
   );
 }
