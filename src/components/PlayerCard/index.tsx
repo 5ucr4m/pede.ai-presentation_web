6b79a76961ab8d5b347609fabfb5d0d25257621f
Marcus Vinicius <5ucr4m@gmail.com>
2024-01-04 20:06:46 -0300

    chore: revel cards

--- src/components/PlayerCard/index.tsx
+++ src/components/PlayerCard/index.tsx
@@ -4,12 +4,29 @@ import cardBack from "../../assets/card-background.png";
 interface PlayerCardProps {
   playerName: string;
   selected: boolean;
+  isRevealed: boolean;
+  value?: string | number;
 }
 
 export const PlayerCard: React.FC<PlayerCardProps> = ({
   playerName,
   selected,
+  isRevealed,
+  value,
 }) => {
+  if (isRevealed) {
+    return (
+      <div className="flex flex-col items-center">
+        <div className="flex items-center justify-center w-16 h-28 bg-gray-200 rounded-md">
+          <p className="text-red-700 text-3xl font-bold text-center capitalize">
+            {value}
+          </p>
+        </div>
+        <p className="text-center mt-2 capitalize">{playerName}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center">
       {selected ? (
